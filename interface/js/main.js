69b9eb928a61935bc3357c36c4f4a2bc288c3e9b
Matthew Bell <logicnotlogical@outlook.com>
2016-02-08 10:45:42 +0000

    updated interface to add button styling

--- interface/js/main.js
+++ interface/js/main.js
@@ -2,10 +2,42 @@ $(document).ready(function() {
     var editor = ace.edit("codeEditor");
     editor.setTheme("ace/theme/cobalt");
     editor.getSession().setMode("ace/mode/javascript");
+    resizeCanvas();
 });
+
+// Triggers on resize.
+$(window).resize(function() {
+    resizeCanvas();
+});
+
+var setMenuButtonIcon = function() {
+    $('#menu-button i').toggleClass('fa-bars');
+    $('#menu-button i').toggleClass('fa-close');
+};
+
 
 // toggles the menu.
 $('#menu-button').click(function() {
     $('nav').toggleClass('active');
     $('.main').toggleClass('active');
+    setMenuButtonIcon();
 });
+
+$('.container,footer,header h1,nav a').click(function() {
+    if ($('nav').hasClass('active')) {
+        $('nav').removeClass('active');
+        $('.main').removeClass('active');
+        setMenuButtonIcon();
+    }
+});
+
+function resizeCanvas(){
+    var con = $("#game-container"),
+    canvas = $("#game-preview")[0],
+    aspect = canvas.height/canvas.width,
+    width = con.width(),
+    height = con.height();
+    console.log('Resizing!');
+    canvas.width = width;
+    canvas.height = Math.round(width * aspect);
+}
