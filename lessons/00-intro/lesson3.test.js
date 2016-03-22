df6b41445e5d927e05114d337f685249e00262e0
Matthew Bell <logicnotlogical@outlook.com>
2016-03-22 14:26:12 +0000

    fixed test 3

--- lessons/00-intro/lesson3.test.js
+++ lessons/00-intro/lesson3.test.js
@@ -1,3 +1,6 @@
 function codeTest() {
-    return (typeof(animal) === 'string' && animal === 'dog'); 
+    loggerdiv.innerHTML = '';
+    consoleOut = [];
+    eval(editor.getValue());
+    return (typeof(animal) === 'string' && animal === 'dog' && consoleOut[0] === 'dog')? "true" : "false";
 }
